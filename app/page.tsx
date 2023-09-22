234f53956c387fe28d92d76c762d6af2fece5329
김민수 MINSOO KIM <alstn113@gmail.com>
2023-09-22 19:30:39 +0900

    fix: dark theme error

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function HomePage() {
-  return <div>Home Page</div>;
-}
+const HomePage = async () => {
+  const data = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(
+    (res) => res.json(),
+  );
+
+  return <div>{JSON.stringify(data)}</div>;
+};
+
+export default HomePage;
