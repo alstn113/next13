d5782c062e5fa9cff4302e8295fd1019c94960fe
김민수 MINSOO KIM <alstn113@gmail.com>
2023-09-22 17:47:44 +0900

    feat: eslint import order

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import { ThemeProvider } from '@/components/providers/theme-provider';
+
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
