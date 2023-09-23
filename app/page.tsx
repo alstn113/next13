c389c0bc2578599c21552ae9d7e507c894c79680
김민수 MINSOO KIM <alstn113@gmail.com>
2023-09-23 15:29:24 +0900

    feat: crud post

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import { BASE_URL } from '@/constants';
-
 const HomePage = async () => {
-  const data = await fetch(`${BASE_URL}`).then((res) => res.json());
-
   return (
     <div>
       <div>Next13 Test Project</div>
-      <div>{JSON.stringify(data)}</div>
     </div>
   );
 };
